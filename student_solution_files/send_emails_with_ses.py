7422eb4ae501c56801b8328cca160a318a529770
S-Oluwapelumi <125037312+S-Oluwapelumi@users.noreply.github.com>
2023-05-05 11:02:43 +0100

    Update send_emails_with_ses.py

--- student_solution_files/send_emails_with_ses.py
+++ student_solution_files/send_emails_with_ses.py
@@ -40,7 +40,7 @@ def lambda_handler(event, context):
     # Replace recipient@example.com with a "To" address. If your account 
     # is still in the sandbox, this address must be verified.
     # --- Insert your code here ---
-    RECIPIENT = dec_dict['email']
+    RECIPIENT = 'shennugao@icloud.com'
     # -----------------------------
 
 
